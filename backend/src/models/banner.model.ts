41e608ea2c844934857906648e3d5d688aa06dee
Kien Le Trung <kienltps36636@fpt.edu.vn.com>
2026-08-19 21:43:27 +0700

    banner model

--- backend/src/models/banner.model.ts
+++ backend/src/models/banner.model.ts
@@ -2,7 +2,32 @@ import mongoose, { Schema, model } from 'mongoose';
 import { IBanner } from '../interfaces/banner.interface.js';
 import { BannerStatus } from '../enums/banner.enum.js';
 
-
+const bannerSchema: Schema<IBanner> = new Schema<IBanner>(
+  {
+    title: {
+      type: String,
+      default: ''
+    },
+    image_url: {
+      type: String,
+      required: true
+    },
+    link_url: {
+      type: String,
+      default: ''
+    },
+    order: {
+      type: Number,
+      default: 0
+    },
+    status: {
+      type: String,
+      enum: BannerStatus,
+      default: BannerStatus.ACTIVE
+    }
+  },
+  { timestamps: true }
+);
 
 const bannerModel = mongoose.models.banner || model('banner', bannerSchema);
 
