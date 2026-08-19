bc218c9888a101338c0936a9766e086124866a2e
Kien Le Trung <kienltps36636@fpt.edu.vn.com>
2026-08-19 21:45:32 +0700

    brad

--- backend/src/models/category.model.ts
+++ backend/src/models/category.model.ts
@@ -2,21 +2,6 @@ import mongoose, { Schema, model } from 'mongoose';
 import { ICategory } from '../interfaces/category.interface.js';
 import { CategoryStatus } from '../enums/category.enum.js';
 
-const categorySchema: Schema<ICategory> = new Schema<ICategory>({
-  name: {
-    type: String,
-    default: ''
-  },
-  description: {
-    type: String,
-    default: ''
-  },
-  status: {
-    type: String,
-    enum: CategoryStatus,
-    default: CategoryStatus.ACTIVE
-  }
-});
 
 const categoryModel = mongoose.models.category || model('category', categorySchema);
 
