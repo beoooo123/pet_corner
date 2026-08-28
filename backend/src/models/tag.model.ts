8fd3e48f96b308cd5dc23996c9715709681f5b02
Kien Le Trung <kienltps36636@fpt.edu.vn.com>
2026-08-28 22:01:23 +0700

    tag.ts

--- backend/src/models/tag.model.ts
+++ backend/src/models/tag.model.ts
@@ -1,7 +1,12 @@
 import mongoose, { Schema, model } from 'mongoose';
 import { ITag } from '../interfaces/tag.interface.js';
 
-
+const tagSchema: Schema<ITag> = new Schema<ITag>({
+  tag_name: {
+    type: String,
+    default: ''
+  }
+});
 
 const tagModel = mongoose.models.tag || model('tag', tagSchema);
 
